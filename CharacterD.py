bc74703cbc7f1a992edc9d967da4975c8993455b
St4te <dogachannel228@gmail.com>
2025-01-24 00:00:47 +0300

    Отрисовка Игрока, Карт, Противника

--- CharacterD.py
+++ CharacterD.py
@@ -1,6 +1,6 @@
 import pygame
 from load_image import load_image
-
+from to_grayscale import to_grayscale
 
 character_group = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
@@ -8,26 +8,29 @@ ui_group = pygame.sprite.Group()
 
 
 class PlayerD(pygame.sprite.Sprite):
-    def __init__(self, id):
+    def __init__(self, obj):
         super().__init__(character_group, all_sprites)
-        self.image = pygame.transform.scale(load_image(id), (300, 248))
+        self.obj = obj
+        self.name = obj.location
+        self.image = pygame.transform.scale(load_image(obj.location), (300, 248))
         self.rect = self.image.get_rect().move(20,216)
 
 
 class EnemyD(pygame.sprite.Sprite):
-    def __init__(self, id):
+    def __init__(self, obj):
         super().__init__(character_group, all_sprites)
-        self.image = pygame.transform.scale(load_image(id), (300, 248))
+        self.obj = obj
+        self.name = obj.location
+        self.image = pygame.transform.scale(load_image(obj.location), (300, 248))
         self.rect = self.image.get_rect().move(1048,216)
 
 
 class UiSkils(pygame.sprite.Sprite):
-    def __init__(self, id):
+    def __init__(self, obj):
         super().__init__(ui_group, all_sprites)
-        self.id = id
-        self.image = load_image(id)
+        self.obj = obj
+        self.image = load_image(obj.location)
         self.rect = self.image.get_rect()
-        self.used = False
 
 
 def load_skills(spisok):
@@ -37,13 +40,13 @@ def load_skills(spisok):
     ui_group.empty()
     place, not_inf = 0, 0
     for card in spisok:
-        skil = UiSkils(f'{card}')
+        skilui = UiSkils(card)
+        skil = card
+        if not skil.can_use:
+            skilui.image = to_grayscale(skilui.image)
         place += 1
         not_inf += 1
-        skil.rect = skil.image.get_rect().move(30 + 149 * (not_inf - 1) + distance * (place - 1), 520)
+        skilui.rect = skilui.image.get_rect().move(30 + 149 * (not_inf - 1) + distance * (place - 1), 520)
         if place == 4:
             place += 5
-
-
-
 
