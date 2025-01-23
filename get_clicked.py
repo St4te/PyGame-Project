e4a6cdd94350583251d1f5de5d9555280e6b441f
St4te <dogachannel228@gmail.com>
2025-01-23 23:48:11 +0300

    Проверка нажатия
    Проверяет нажат ли Enter или спрайт

--- get_clicked.py
+++ get_clicked.py
@@ -1,19 +1,51 @@
 import pygame
-from to_grayscale import to_grayscale
 
 
 def get_clicked(group, mouse_pos):
     pass
 
 
-def clicked_sprite(group, mouse_pos):
+'''def clicked_sprite(group, mouse_pos):
     for sprite in group:
         rect = sprite.rect
         if rect.collidepoint(mouse_pos[0], mouse_pos[1]) and sprite.used == False:
             sprite.image = to_grayscale(sprite.image)
-            sprite.used = True
-        #return [True, sprite.id]
+            sprite.used = True'''
 
 
-def clicked_endBtn(mouse_pos):
-    pass
+'''def clicked_sprite(group, mouse_pos):
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for sprite in group:
+                rect = sprite.rect
+                if rect.collidepoint(mouse_pos[0], mouse_pos[1]):
+                    return sprite.obj
+    return False
+
+
+
+def clicked_endBtn():
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                return True
+    return False'''
+
+
+def clicked_sprite(group, mouse_pos):
+    index = -1
+    for sprite in group:
+        index += 1
+        rect = sprite.rect
+        if rect.collidepoint(mouse_pos[0], mouse_pos[1]):
+            print(index)
+            return str(index)
+    return False
+
+
+def clicked_endBtn():
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                return True
+    return False
