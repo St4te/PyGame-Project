8358db21395d2d9f16ff799c095070a60bf2b311
St4te <dogachannel228@gmail.com>
2025-01-24 00:01:27 +0300

    Класс Игрока

--- Character.py
+++ Character.py
@@ -1,14 +1,20 @@
 import sqlite3
 from random import choices
+from Card import *
 
 
 class Character:
     def __init__(self, curse, artifacts, mana, card1_id, card2_id, card3_id, card4_id):
         self.curse = curse
+        self.location = 'warrior-clipart-lg.png'
         self.artifacts = artifacts
         self.max_mana = mana
         self.mana = mana
-        self.permanent_cards = [Card(card1_id), Card(card2_id), Card(card3_id), Card(card4_id)]
+        self.card_1 = card1_id
+        self.card_2 = card2_id
+        self.card_3 = card3_id
+        self.card_4 = card4_id
+        #self.permanent_cards = [Card(card1_id), Card(card2_id), Card(card3_id), Card(card4_id)]
         self.hp = 50
         self.max_hp = 50
         self.shield = 0
@@ -22,26 +28,39 @@ class Character:
         b = []
         for i in res:
             a.append(i[0])
-
             b.append(4 - i[1])
         con.close()
         c = choices(a, weights=b, k=3)
-        return [self.permanent_cards, [Card(c[0]), Card(c[1]), Card(c[2])]]
+        return [Card(self.card_1), Card(self.card_2), Card(self.card_3), Card(self.card_4), Card(c[0]), Card(c[1]), Card(c[2])]
 
-    def attack(self, card, enemy, cards):
-        if card.mana > self.mana:
+    def attack(self, card_index, enemy, cards):
+        card_index = int(card_index)
+        print(card_index)
+        if cards[card_index].mana > self.mana:
             return cards
-        card.apply_card(self, enemy)
-        a = [[], []]
-        for i in cards[0]:
-            if i.id != card.id:
-                a[0].append(i)
-        for i in cards[1]:
-            if i.id != card.id:
-                a[1].append(i)
-        return a
+        cards[card_index].apply_card(self, enemy)
+        cards[card_index].use()
+        return cards
 
     def is_live(self):
         if self.hp <= 0:
             return False
         return True
+
+
+
+    '''def attack(self, card_index, enemy, cards):
+        if cards[card_index].mana > self.mana:
+            return cards
+        cards[card_index].apply_card(self, enemy)
+        cards[card_index].use()
+        return cards'''
+
+    '''def attack(self, card, enemy, cards):
+        if card.mana > self.mana:
+            return cards
+        card.apply_card(self, enemy)
+        for i in cards:
+            if card.id == i.id:
+                i.use()
+        return cards'''
