ea20b4aa9d8d117ec767e89a9ac9d4b60ebc0044
St4te <dogachannel228@gmail.com>
2025-01-23 23:59:53 +0300

    Класс противника

--- Enemy.py
+++ Enemy.py
@@ -1,11 +1,12 @@
 class Enemy:
-    def __init__(self, max_hp, hp, shield, protection, curse, artifacts):
+    def __init__(self, max_hp, hp, shield, protection, curse, artifacts, location):
         self.hp = hp
         self.max_hp = max_hp
         self.protection = protection
         self.shield = shield
         self.curse = curse
         self.artifacts = artifacts
+        self.location = location
 
 
     def attack(self):
