bf3d7fb26bad73e826cd4197478cf6752f6602e0
St4te <dogachannel228@gmail.com>
2025-01-30 23:16:16 +0300

    Атака противника

--- Kirpich.py
+++ Kirpich.py
@@ -4,22 +4,45 @@ from Enemy import *
 
 
 class Kirpich(Enemy):
-    def __init__(self, curse, artifacts):
-        super().__init__(20, 20, 0, 4, curse, artifacts, 'werewolf-clipart-lg.png')
+    def __init__(self, curse, artifacts, element):
+        super().__init__(20, 20, 0, 4, curse, artifacts, 'werewolf-clipart-lg.png', element)
 
     def attack(self, character):
         character.mana = character.max_mana
         self.shield = 0
+
+
         a = randint(1, 100)
         if a <= 60:
             self.pattern1(character)
-        elif a < 100:
+        elif a < 95:
             self.pattern2()
         else:
             self.pattern3(character)
 
+
+        if self.element == 1:
+            damage = 1
+            if damage <= character.shield:
+                character.shield -= damage
+            else:
+                character.hp += character.shield - damage
+                character.shield = 0
+        elif self.element == 2:
+            self.hp += 1
+            if self.hp > self.max_hp:
+                self.hp = self.max_hp
+        elif self.element == 3:
+            self.shield += 1
+
+
     def pattern1(self, character):
-        character.hp -= 10
+        damage = 10
+        if damage <= character.shield:
+            character.shield -= damage
+        else:
+            character.hp += character.shield - damage
+            character.shield = 0
 
     def pattern2(self):
         self.hp += 1
@@ -28,4 +51,9 @@ class Kirpich(Enemy):
         self.shield = 3
 
     def pattern3(self, character):
-        character.hp -= 100
+        damage = 100
+        if damage <= character.shield:
+            character.shield -= damage
+        else:
+            character.hp += character.shield - damage
+            character.shield = 0
