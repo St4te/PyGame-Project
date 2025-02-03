f0c5646655649a6e51b6308e258b80ec6d71dd1f
St4te <dogachannel228@gmail.com>
2025-02-03 18:04:57 +0300

    Здесь вся игра

--- Map.py
+++ Map.py
@@ -1,4 +1,6 @@
 import pygame
+import sqlite3
+from random import randint
 import sys
 from Card import *
 from Character import *
@@ -16,11 +18,16 @@ from Stats import *
 from get_MousePos import *
 from Fight import *
 from Shop import *
+from Progress import draw_progress
 
 
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption('Игра')
+    pygame.mixer.init()
+    pygame.mixer.music.load('Music/музяка.mp3')
+    pygame.mixer.music.set_volume(0.1)
+    pygame.mixer.music.play(-1)
+    pygame.display.set_caption('KAT (Knight And Throne)')
     size = WIDTH, HEIGHT = 1366, 768
     screen = pygame.display.set_mode(size)
     screen.fill((0, 0, 0))
@@ -46,27 +53,37 @@ if __name__ == '__main__':
                     sys.exit()
 
         character = Character(0, 0, 3, 35, 35)
-        coins = 0
-        stage = 1
-        level = 1
         a = []
-        element = 1
+        element = randint(1, 4)
         qwerty24 = True
         while qwerty24:
-            if stage % 2 == 1:
-                monster = det_rand_enemy_id(a, False)
-                a.append(monster)
-                fight = Fight(3, None, monster, character, None, 1)
-                qwerty24 = fight.start(screen)
-                coins += 50
-                stage += 1
-                if stage == 6:
-                    a = []
-                    stage = 1
-                    level += 1
-                print(stage)
+            if character.stage % 2 == 1:
+                if character.stage == 5:
+                    monster = det_rand_enemy_id(a, True)
+                    a.append(monster)
+                    fight = Fight(3, None, monster, character, None, element)
+                    qwerty24 = fight.start(screen)
+                    character.coins += 50
+                    if qwerty24:
+                        draw_progress(screen, character)
+                    character.stage += 1
+                else:
+                    monster = det_rand_enemy_id(a, False)
+                    a.append(monster)
+                    fight = Fight(3, None, monster, character, None, element)
+                    qwerty24 = fight.start(screen)
+                    character.coins += 50
+                    if qwerty24:
+                        draw_progress(screen, character)
+                    character.stage += 1
             else:
                 buy_group = buy_group
-                draw_shop(screen, character, coins, buy_group)
-                stage += 1
+                draw_shop(screen, character, buy_group)
+                draw_progress(screen, character)
+                character.stage += 1
+            if character.stage == 6:
+                a = []
+                character.level += 1
+                character.stage = 1
+                element = randint(1, 4)
     pygame.quit()
