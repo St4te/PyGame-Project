e776df058e37944c7636983ce80db628c1554e4f
St4te <dogachannel228@gmail.com>
2025-02-03 18:09:09 +0300

    Проверка нажатий

--- get_clicked.py
+++ get_clicked.py
@@ -1,35 +1,33 @@
 import pygame
+from to_grayscale import to_grayscale
+from Stats import draw_Hp, draw_Mana
 
 
-def get_clicked(group, mouse_pos):
-    pass
-
-
-'''def clicked_sprite(group, mouse_pos):
+def clicked_shop(screen, group, character, mouse_pos):
+    index = -1
     for sprite in group:
+        index += 1
         rect = sprite.rect
-        if rect.collidepoint(mouse_pos[0], mouse_pos[1]) and sprite.used == False:
-            sprite.image = to_grayscale(sprite.image)
-            sprite.used = True'''
-
-
-'''def clicked_sprite(group, mouse_pos):
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            for sprite in group:
-                rect = sprite.rect
-                if rect.collidepoint(mouse_pos[0], mouse_pos[1]):
-                    return sprite.obj
-    return False
-
-
-
-def clicked_endBtn():
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
-                return True
-    return False'''
+        if rect.collidepoint(mouse_pos[0], mouse_pos[1]):
+            if index == 0:
+                if sprite.uses:
+                    sprite.uses -= 1
+                    character.buy(30, 1, 0, 0)
+            elif index == 1:
+                if sprite.uses:
+                    sprite.uses -= 1
+                    character.buy(30, 0, 0, 1)
+            elif index == 2:
+                if sprite.uses:
+                    sprite.uses -= 1
+                    character.buy(30, 0, 2, 0)
+    group.draw(screen)
+    font = pygame.font.Font(None, 50)
+    text = font.render(f'Монеты: {character.coins}', True, (255, 255, 255))
+    screen.blit(text, (40, 140))
+    draw_Hp(screen, character)
+    draw_Mana(screen, character)
+    pygame.display.flip()
 
 
 def clicked_sprite(group, mouse_pos):
