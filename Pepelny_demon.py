92b41935dddf1ac41153704363e41d23a457ece5
St4te <dogachannel228@gmail.com>
2025-02-03 18:06:05 +0300

    Класс 1 Босса

--- Pepelny_demon.py
+++ Pepelny_demon.py
@@ -1,19 +1,50 @@
 from random import randint
+from Enemy import *
+from Character import *
 
-class Pepelny_demon:
-    def __init__(self, curse, artifacts):
-        super().__init__(20, 20, 0, 4, curse, artifacts, 'werewolf-clipart-lg.png')
+class Pepelny_demon(Enemy):
+    def __init__(self, curse, artifacts, element):
+        super().__init__(60, 60, 0, 3, curse, artifacts, 'werewolf-clipart-lg.png', element)
 
     def attack(self, character):
-        character.mana = character.max_mana
         self.shield = 0
         a = randint(1, 100)
-        if a <= 20:
+        if a <= 40:
             self.pattern1(character)
-        elif a <= 60:
+        elif a <= 80:
             self.pattern2(character)
         else:
             self.pattern3(character)
 
     def pattern1(self, character):
-        pass
+        damage = 7
+        if damage <= character.shield:
+            character.shield -= damage
+        else:
+            character.hp += character.shield - damage
+            character.shield = 0
+        self.shield = 4
+
+    def pattern2(self, character):
+        damage = 0
+        for i in range(4):
+            if character.prev_cards[i].can_use:
+                damage += character.prev_cards[i].damage
+        if damage != 0:
+            if damage <= character.shield:
+                character.shield -= damage
+            else:
+                character.hp += character.shield - damage
+                character.shield = 0
+        else:
+            self.pattern1(character)
+
+    def pattern3(self, character):
+        heal = 0
+        for i in character.cards:
+            heal += i.mana
+        heal *= 2
+        self.hp += heal
+        if self.hp > self.max_hp:
+            self.hp = self.max_hp
+        self.shield = 4
